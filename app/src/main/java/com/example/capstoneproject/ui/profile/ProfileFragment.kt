630b2c1783ad8a8f86aba4efe2971898d69d38c7
FaishalFaruq02 <faruq.faishal25@gmail.com>
2024-11-21 19:34:41 +0700

    Initial Commit

---
+++ app/src/main/java/com/example/capstoneproject/ui/profile/ProfileFragment.kt
@@ -0,0 +1,18 @@
+package com.example.capstoneproject.ui.profile
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.capstoneproject.R
+
+class ProfileFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate layout untuk fragment
+        return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+}
